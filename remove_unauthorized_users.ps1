6a14e96305044f9d700f43881cbada3c609ca2a4
Xuejian Sundvall <jiansundvall@gmail.com>
2025-09-24 20:48:39 -0400

    add remove unauthorized users ps1

---
+++ remove_unauthorized_users.ps1
@@ -0,0 +1,21 @@
+# Define authorized users
+$authorizedUsers = @("Administrator", "Guest", "krbtgt", "jians", "jsmith")
+
+# Get all local users
+$localUsers = Get-LocalUser | Where-Object { $_.Name -notlike '*$' }
+
+# Filter out authorized users
+$unauthorizedUsers = $localUsers | Where-Object {
+    $authorizedUsers -notcontains $_.Name -and $_.Enabled -eq $true
+}
+
+# Log and remove unauthorized users
+foreach ($user in $unauthorizedUsers) {
+    Write-Host "Removing unauthorized user: $($user.Name)"
+    try {
+        Remove-LocalUser -Name $user.Name
+        Write-Host "Successfully removed $($user.Name)"
+    } catch {
+        Write-Warning "Failed to remove $($user.Name): $_"
+    }
+}
